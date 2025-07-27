9b93112c8cfba4a82376ccc7437a2d2d799bc073
gim <ugisugiman6@gmail.com>
2025-07-27 16:33:25 +0700

    cek 4

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -21,7 +21,7 @@ router.post('/demote-member/:id', ensureAuthenticated, ensureRole('admin'), admi
 
 // Routes edit
 router.get('/book-edits', ensureAuthenticated, ensureRole('admin'), adminController.getPendingEdits);
-router.post('/approve-edit/:id', ensureAuthenticated, ensureRole('admin'), adminController.approveBookEdit);
-router.post('/reject-edit/:id', ensureAuthenticated, ensureRole('admin'), adminController.rejectBookEdit);
+router.post('/book-edits/:id/approve', adminController.approveBookEdit);
+router.post('/book-edits/:id/reject', adminController.rejectBookEdit);
 
 module.exports = router;
